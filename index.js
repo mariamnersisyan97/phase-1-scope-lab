729e610218a021b57f657307eda52a285b662e5b
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-03-17 17:39:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'some value';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'some other value';
+
+}
+
+//   describe('changeLeastFavoriteCustomer()', function() {
+//     it('unsuccessfully tries to reassign the least favorite customer', function() {
+//       expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+//     });
+//   });
+// });
